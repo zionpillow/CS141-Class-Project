6bdd3eab9dda8487f6bb8f8034f90f248e634e4e
zionpillow <natanael_ariawan@hotmail.com>
2016-05-22 04:28:49 -0700

    Add files via upload

---
+++ ClassProjectUpdate_5-22-16/src/edu/cpp/cs/cs141/classproject/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.classproject;
+
+/**
+ * @author Aidan Novobilski
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GameEngine game = new GameEngine(new UI());
+		
+		game.start();
+	}
+
+}
